938cde6e3516a3abcf639a50bf35f643b3c5c14d
DaRkFox <cristian14rap@gmail.com>
2018-06-22 22:50:25 +0200

    First Commit

---
+++ ConquerMonsterTranslator/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ConquerMonsterTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Files/SQL_Test.sql";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 212);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "R E LO A D";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Monster.txt - Names";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 53);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(209, 212);
+            this.listBox2.TabIndex = 6;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(235, 53);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(209, 212);
+            this.listBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "SQL - Names";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Convert SQL Names to Monster.dat Names";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 270);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ConquerMonsterTranslator By DaRkHC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ConquerMonsterTranslator/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConquerMonsterTranslator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string TempData;
+        public static string NameMonsters;
+        public static string NameMonstersES;
+        public static string[] NameMonstersSQL;
+        public static string NameMonstersSQLX;
+        public static string IDMonster;
+        public static string OldNameMonsterForEdit;
+        public static string OldLookFaceForEdit;
+        public static string NameMonsterForEdit;
+        public static string LookFaceForEdit;
+        public static string LineNameMonsterForEdit;
+        public static string LineLookFaceForEdit;
+        public static string LineForSave;
+
+        public void TranslateSQL(string Path)
+        {
+            string[] tempdata = System.IO.File.ReadAllLines(Path);
+            foreach (string tempdatax in tempdata)
+            {
+                string[] readsql = tempdatax.Split(',');
+                IDMonster = readsql[0].Substring(readsql[0].IndexOf("'"), readsql[0].Length - readsql[0].IndexOf("'"));
+                SearchNameES(NameMonsterForEdit, LookFaceForEdit);
+                OldNameMonsterForEdit = readsql[1];
+                OldLookFaceForEdit = readsql[4];
+                readsql[1] = "'" + NameMonsterForEdit.Trim(new Char[] { '[', ']' }) + "'";
+                readsql[4] = "'" + LookFaceForEdit.Trim('M', 'a', 'x', 'L', 'i', 'f', 'e', '=') + "'";
+                LineNameMonsterForEdit = readsql[1];
+                LineLookFaceForEdit = readsql[4];
+                LineForSave += tempdatax + Environment.NewLine;
+                LineForSave = LineForSave.Replace(OldNameMonsterForEdit, LineNameMonsterForEdit);
+                LineForSave = LineForSave.Replace(OldLookFaceForEdit, LineLookFaceForEdit);
+            }
+            System.IO.File.WriteAllText(Path, LineForSave);
+        }
+
+        public void SearchNameES(string value, string value2)
+        {
+            int n = 0;
+            string[] tempdata = System.IO.File.ReadAllLines("Files/MonsterES.txt");
+            foreach (string tempdatax in tempdata)
+            {
+                if (tempdatax.Contains("TypeID=" + IDMonster.Replace("'","")))
+                {
+                    int n2 = n - 12;
+                    value = tempdata[n2];
+                    value2 = tempdata[n2+3];
+                }
+                n++;
+            }
+            NameMonsterForEdit = value;
+            LookFaceForEdit = value2;
+        }
+
+        public void LoadNamesSQL()
+        {
+            string[] tempdata = System.IO.File.ReadAllLines(textBox1.Text);
+            foreach (string tempdatax in tempdata)
+            {
+                string[] readsql = tempdatax.Split(',');
+                readsql[1] = readsql[1].Replace("'", "");
+                listBox3.Items.Add(readsql[1].Trim());
+            }
+        }
+
+        public void LoadNames(string Path, ListBox listBox)
+        {
+            string[] tempdata = System.IO.File.ReadAllLines(Path);
+            foreach (string tempdatax in tempdata)
+            {
+                if (tempdatax.Contains("[") && tempdatax.Contains("]"))
+                {
+                    TempData = tempdatax;
+                    TempData = TempData.Replace("[", "");
+                    TempData = TempData.Replace("]", "");
+                    NameMonsters = Environment.NewLine + TempData;
+                    listBox.Items.Add(NameMonsters.Trim());
+                    NameMonstersES += NameMonsters;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TranslateSQL(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadNames("Files/MonsterES.txt", listBox2);
+            LoadNamesSQL();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadNames("Files/MonsterES.txt", listBox2);
+            LoadNamesSQL();
+        }
+    }
+}
